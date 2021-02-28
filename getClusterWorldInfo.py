5358bfe6288fbdd2682d98b4135d9028734161fd
te26Oku <teturou@aw.wakwak.com>
2021-02-28 20:43:06 +0900

    整形済みのデータをcsvに書き込む処理を追加

--- getClusterWorldInfo.py
+++ getClusterWorldInfo.py
@@ -1,5 +1,6 @@
 from requests_html import HTMLSession
 import csv
+import trimData
 
 url = "https://cluster.mu/w/4ec2b9d7-f287-4347-8dfe-338fa2738b2f"
 
@@ -13,19 +14,16 @@ h6 = r.html.find('h6')
 info_list = r.html.find('.MuiList-root')
 p = info_list[0].find('p')
 
-name = h2[0].text
-like = h6[0].text
-creator = h6[1].text
-size = p[1].text
-date = p[3].text
+name_r = h2[0].text
+like_r = h6[0].text
+creator_r = h6[1].text
+size_r = p[1].text
+date_r = p[3].text
 
 if len(info_list) > 0:
-    print(name)
-    print(like)
-    print(creator)
-    print(size)
-    print(date)
-    row = [name, like, creator, size, date]
+    row = trimData.trim_all_data(name_r, like_r, creator_r, size_r, date_r)
+    print(row)
+
 with open('data/sample.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(row)
