db45a0209ab32c83a5ad958d464671b4b0d5908e
te26Oku <teturou@aw.wakwak.com>
2021-02-28 11:59:41 +0900

    ワールド詳細ページから各種情報を取得する処理を作成

---
+++ getClusterWorldInfo.py
@@ -0,0 +1,26 @@
+from requests_html import HTMLSession
+
+url = "https://cluster.mu/w/4ec2b9d7-f287-4347-8dfe-338fa2738b2f"
+
+session = HTMLSession()
+r = session.get(url)
+
+r.html.render()
+
+h2 = r.html.find('h2')
+h6 = r.html.find('h6')
+info_list = r.html.find('.MuiList-root')
+p = info_list[0].find('p')
+
+name = h2[0].text
+like = h6[0].text
+creator = h6[1].text
+size = p[1].text
+date = p[3].text
+
+if len(info_list) > 0:
+    print(name)
+    print(like)
+    print(creator)
+    print(size)
+    print(date)
