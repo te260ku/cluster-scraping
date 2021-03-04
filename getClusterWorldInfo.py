43d3f8eca576bd181c362c54e32d7540eee9feb2
te26Oku <teturou@aw.wakwak.com>
2021-03-04 21:01:56 +0900

    ワールド詳細ページのプレイ数といいね数の表示仕様の変更に対応した

--- getClusterWorldInfo.py
+++ getClusterWorldInfo.py
@@ -3,12 +3,10 @@ import time
 import csv
 import trimData
 
-url = "https://cluster.mu/w/c99e7eab-2d28-40c5-a125-f1fa8aa8c269"
+url = "https://cluster.mu/w/79e26b4f-d494-40b3-981f-ce4ca1e2a6ac"
 
 session = HTMLSession()
 r = session.get(url)
-
-
 
 r.html.render()
 
@@ -16,19 +14,18 @@ r.html.render()
 h2 = r.html.find('h2')
 h6 = r.html.find('h6')
 info_list = r.html.find('.MuiList-root')
-p = info_list[0].find('p')
+play_list = r.html.find('.WorldStats__Container-sc-17lod9z-0')
+p_play = play_list[0].find('p')
+p_info = info_list[0].find('p')
 
 name_r = h2[0].text
-like_r = h6[0].text
-creator_r = h6[1].text
-size_r = p[1].text
-date_r = p[3].text
+like_r = p_play[0].text
+play_r = p_play[1].text
+creator_r = h6[0].text
+size_r = p_info[1].text
+date_r = p_info[3].text
 
 if len(info_list) > 0:
-    row = trimData.trim_all_data(name_r, like_r, creator_r, size_r, date_r)
+    row = [name_r, like_r, play_r, creator_r, size_r, date_r]
     print(row)
-
-with open('data/sample.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(row)
     
