ec0abe0dae0b4ffdeadebc59204b16d0491ff1bd
te26Oku <teturou@aw.wakwak.com>
2021-03-06 22:03:35 +0900

    レンダリング時のスリープ時間を設定した

--- getClusterWorldInfoWithTransition.py
+++ getClusterWorldInfoWithTransition.py
@@ -1,75 +1,102 @@
 from requests_html import HTMLSession
+from requests_html import AsyncHTMLSession
 import time
 import csv
 
 url_base = "https://cluster.mu/w?page="
 base = "https://cluster.mu"
-start_page_num = 2
-end_page_num = 5
+start_page_num = 50
+end_page_num = 55
+
+# def wait_render(u):
+#     assesion = AsyncHTMLSession()
+
+#     async def process():
+#         r = await assesion.get(u)
+#         await r.html.arender(wait=5, sleep=5)
+#         return r
+
+#     r = assesion.run(process)[0]
+#     cards = r.html.find('.MuiCardActionArea-root')
+
+#     if len(cards) > 0:
+#         print(len(cards))
+#     else:
+#         print("error")
+
 
 for i in range(start_page_num, end_page_num):
-    url = url_base + str(i)
-    links = []
-    count = 0
+    try:
+        url = url_base + str(i)
+        links = []
+        count = 0
 
-    session = HTMLSession()
-    r = session.get(url)
-    time.sleep(1)
-    r.html.render()
-    time.sleep(1)
+        # wait_render(url)
 
-    cards = r.html.find('.MuiCardActionArea-root')
-    
-    del session
-    del r
+        session = HTMLSession()
+        r = session.get(url)
+        r.html.render(wait=5, sleep=5)
 
-    if len(cards) > 0:
+        cards = r.html.find('.MuiCardActionArea-root')
+        # time.sleep(5)
         
-        for link in cards:
-            tmp = base + link.attrs['href']
-            links.append(tmp)
-        
-        print(len(links))
+        if len(cards) > 0:
+            print(url)
+        else:
+            print("error")
 
-        with open('data/sample.csv', 'a') as f:
-            writer = csv.writer(f)
+        if len(cards) > 0:
+            print(url)
+            for link in cards:
+                tmp = base + link.attrs['href']
+                links.append(tmp)
             
-            for link in links:
-                try:
-                    
-                    session_link = HTMLSession()
-                    r_ = session_link.get(link)
-                    r_.html.render()
+            # print(len(links))
 
-                    h2 = r_.html.find('h2')
-                    h6 = r_.html.find('h6')
-                    info_list = r_.html.find('.MuiList-root')
-                    p = info_list[0].find('p')
-
-                    del session_link
-                    del r_
-
-                    name_r = h2[0].text
-                    if len(h6) == 2:
-                        like_r = h6[0].text
-                        creator_r = h6[1].text
-                    else:
-                        like_r = "0 いいね"
-                        creator_r = h6[0].text
-                    size_r = p[1].text
-                    date_r = p[3].text
-
-                    row = [name_r, like_r, creator_r, size_r, date_r]
-
-                    count += 1
-                    print(count)
-
-                    writer.writerow(row)
-                    print(row)
-
-                    time.sleep(2)
+            
+            with open('data/sample-1.csv', 'a') as f:
+                writer = csv.writer(f)
+                for link in links:
+                    try:
                         
-                except:
-                    pass
-    else:
-        print("error")
+                        session_link = HTMLSession()
+                        r_ = session_link.get(link)
+                        r_.html.render(wait=5, sleep=5)
+
+
+                        h2 = r_.html.find('h2')
+                        h6 = r_.html.find('h6')
+                        info_list = r_.html.find('.MuiList-root')
+                        play_list = r_.html.find('.WorldStats__Container-sc-17lod9z-0')
+                        p_play = play_list[0].find('p')
+                        p_info = info_list[0].find('p')
+
+                        
+
+                        
+
+
+                        name_r = h2[0].text
+                        play_r = p_play[0].text
+                        like_r = p_play[1].text
+                        creator_r = h6[0].text
+                        size_r = p_info[1].text
+                        date_r = p_info[3].text
+
+
+
+                        row = [name_r, play_r, like_r, creator_r, size_r, date_r]
+
+                        count += 1
+                        print(count)
+                        
+                        
+                        writer.writerow(row)
+                        time.sleep(2)
+
+                        
+                        
+                    except:
+                        pass
+    except:
+        pass
